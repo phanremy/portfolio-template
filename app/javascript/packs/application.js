f4b6e22ac89b1b429751b038554f4602f311bb9d
Remy Phan <phan.remy@gmail.com>
2020-02-18 19:59:35 +0100

    tabs created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,24 @@
 import "bootstrap";
+
+const selectorShow = () => {
+  const achievements = document.getElementById('achievements-selector');
+  const currentWorks = document.getElementById('current-works-selector');
+  const achievementsContainer = document.getElementById('achievements');
+  const currentWorksContainer = document.getElementById('current-works');
+  if (achievements && currentWorks) {
+    achievements.addEventListener('click', (event) => {
+      achievements.classList.add('active');
+      currentWorks.classList.remove('active');
+      achievementsContainer.classList.remove('hidden');
+      currentWorksContainer.classList.add('hidden');
+    });
+    currentWorks.addEventListener('click', (event) => {
+      achievements.classList.remove('active');
+      currentWorks.classList.add('active');
+      achievementsContainer.classList.add('hidden');
+      currentWorksContainer.classList.remove('hidden');
+    });
+  }
+}
+
+selectorShow();
